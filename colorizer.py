f0a47525bf4c4bea6b86cff7bb864b6bcdb53300
Bailey Chen <bchen919@gmail.com>
2018-12-11 04:47:07 -0500

    setting up data

---
+++ colorizer.py
@@ -0,0 +1,20 @@
+from nn_model import model, node
+from colorizer_helper import load_image
+
+from PIL import Image
+
+class colorizer:
+    """
+    """
+
+    def __init__(self):
+        self.model = model()
+        self.image =
+
+    def train(self):
+        pass
+
+
+if __name__ == '__main__':
+    IMAGE = load_image("./test.jpg")
+    MODEL =  colorizer()
