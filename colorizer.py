76928a0cbf375620bd29139624b13cf1d70e648c
Bailey Chen <bchen919@gmail.com>
2018-12-12 05:17:59 -0500

    added sigmoid activation

--- colorizer.py
+++ colorizer.py
@@ -1,15 +1,13 @@
-from nn_model import model, nn_node
+from nn_model import model
 import numpy as np
 from PIL import Image
-from random import randint
 import os
 
-class colorizer:
 
+class colorizer:
     def __init__(self):
         self.model = model()
         self.import_data("./training/")
-
 
     def train(self):
         # get training data and grayscale image from utils
@@ -25,35 +23,32 @@ class colorizer:
 
     def import_data(self, path):
         print("Importing training files:")
-        X = np.empty((0,9),int)
-        Y = np.empty((0,3),int)
+        X = np.empty((0, 9), int)
+        Y = np.empty((0, 3), int)
         for filename in os.listdir(path):
             print(f"Importing {filename}...")
             im = Image.open(path+filename)
             im.load()
             rgb_data = np.asarray(im, dtype="int32")
             gray = colorizer.rgb_to_grayscale(rgb_data)
-            padded_gray = np.pad(gray, (1, 1), "constant", constant_values = 0)
+            padded_gray = np.pad(gray, (1, 1), "constant", constant_values=0)
             i_size, j_size = np.shape(gray)
-            im_X = np.empty((gray.size,9),int)
-            im_Y = np.empty((gray.size,3),int)
+            im_X = np.empty((gray.size, 9), int)
+            im_Y = np.empty((gray.size, 3), int)
             index = 0
             for i in range(i_size):
                 for j in range(j_size):
                     im_X[index] = (padded_gray[i:i+3, j:j+3].flatten())
                     im_Y[index] = (rgb_data[i, j, 0:3])
                     index += 1
-            X = np.concatenate((X ,im_X))
-            Y = np.concatenate((Y ,im_Y))
-            print  (f"Imported {filename}: {gray.size} samples imported")
+            X = np.concatenate((X, im_X))
+            Y = np.concatenate((Y, im_Y))
+            print(f"Imported {filename}: {gray.size} samples imported")
         self.X = np.array(X)
         self.Y = np.array(Y)
 
     @staticmethod
     def rgb_to_grayscale(data):
-        r = data[:,:,0]
-        g = data[:,:,1]
-        b = data[:,:,2]
         grayscale = [0.3, 0.55, 0.15]
         gray = data[:, :, 0:3].dot(grayscale)
         gray = np.floor(gray)
@@ -63,5 +58,6 @@ class colorizer:
 
 
 if __name__ == '__main__':
-    test =  colorizer()
+    test = colorizer()
+    print(test.model.forward(test.X[0], test.Y[0]), training=True)
     print("finished")
--- nn_model.py
+++ nn_model.py
@@ -1,5 +1,8 @@
 import numpy as np
-from random import random
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
 class model:
@@ -8,57 +11,38 @@ class model:
         input_size = 9
         h1_out_size = 5
         o_out_size = 3
-        self.weights_o = np.random.rand(o_out_size,h1_out_size)
+        self.weights_o = np.random.rand(h1_out_size, o_out_size,)
         self.bias_o = np.random.rand(o_out_size)
-        self.weights_h1 = np.random.rand(h1_out_size, input_size)
-        self.bias_h1 = np.random.rand(o_out_size)
+        self.weights_h1 = np.random.rand(input_size, h1_out_size)
+        self.bias_h1 = np.random.rand(h1_out_size)
 
     def next_training_sample(self):
         # TODO fetch random input
         pass
 
-    def forward(self, input):
-        flat_input = input.flatten()
+    def forward(self, x, y, training=False):
+        # input x (9,)
+        out_h1 = np.dot(x, self.weights_h1)
+        out_h1 += self.bias_h1
+        out_h1 = sigmoid(out_h1)
+        out_o = np.dot(out_h1, self.weights_o)
+        out_o += self.bias_o
+        out_o = sigmoid(out_o)
 
+        if training:
+            loss = (np.sum(out_o - y)**2)
+            self.update_output_layer(loss, out_h1, out_o)
+            self.update_hidden_layer1(loss, x, out_o)
 
+        return out_o
 
-    def loss(output, actual):
-        error = (np.sum(output-actual)**2)
-        return error
-
-    def update_hidden_layer(self, layer):
+    def update_hidden_layer1(self, layer, in_j, out_j):
+        # TODO error back propagation
         pass
 
-    def update_output_layer(self, loss):
-
-        for node in self.outputl:
-            for j in range(node.input_size):
-                in_k = node.weighted_sum
-                out_k = node.out
-                gradient = - self.alpha*(out_k*(1-out_k))*out_k  # * ?out_k
-                node.weights[j] -= gradient
+    def update_output_layer(self, loss, in_k, out_k):
+        in_size, out_size = self.weights_o.shape
+        for k in range(out_size):
+            gradient = self.alpha*(out_k*(1-out_k))*out_k  # * ?out_k
+            self.weights_o[k] -= gradient
             # node.bias = node.weight - self.alpha*(loss*(1-loss))
-
-
-class nn_node:
-    def __init__(self):
-        self.weights = None
-        self.bias = None
-        self.input_size = 0
-        self.inputs = []
-        self.out = None
-        self.weighted_sum = None
-
-    @staticmethod
-    def random_init(n):
-        node = nn_node()
-        node.input_size = n
-        node.weight = [random() for _ in range(node.input_size)]
-        node.bias = random()
-        return node
-
-    def get_output(self, inputs):
-        y = inputs * self.weights + self.bias
-        y = np.sum(y)
-        output = 1.0 / (1 + np.exp(-y))
-        return output
